0819bc63f45bf708d8dbe1842d2a3210688b14b7
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-05-02 01:22:26 +0200

    Work on RecyclerView

--- app/src/main/java/android/benedetto/com/newsapp2/data/NewsArticlePagerAdapter.java
+++ app/src/main/java/android/benedetto/com/newsapp2/data/NewsArticlePagerAdapter.java
@@ -3,14 +3,16 @@ package android.benedetto.com.newsapp2.data;
 import android.benedetto.com.newsapp2.R;
 import android.benedetto.com.newsapp2.model.NewsArticle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class NewsArticlePagerAdapter extends
-        RecyclerView.Adapter<NewsArticlePagerAdapter.ViewHolder> {
+        RecyclerView.Adapter<NewsArticlePagerAdapter.MyViewHolder> {
     private List<NewsArticle> news;
 
     // Provide a suitable constructor (depends on the kind of dataset)
@@ -21,34 +23,34 @@ public class NewsArticlePagerAdapter extends
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
+        TextView itemTitleTV;
 
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            itemTitleTV = (TextView) itemView.findViewById(R.id.item_title);
         }
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public NewsArticlePagerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        View v = (View) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.news_item, parent, false);
-        ViewHolder vh = new ViewHolder(v);
+        MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(news.get(position).getTitle());
-
+        Log.v("onBindViewHolder", "position: " + position);
+        NewsArticle na = news.get(position);
+        holder.itemTitleTV.setText(na.getTitle());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
