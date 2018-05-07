8251bb220fe510104c7692b6be2a10193c244876
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-05-07 22:17:39 +0200

    Fix indentation

--- app/src/main/java/android/benedetto/com/newsapp2/controller/AppController.java
+++ app/src/main/java/android/benedetto/com/newsapp2/controller/AppController.java
@@ -10,66 +10,65 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
-        public static final String TAG = AppController.class.getSimpleName();
-        private static AppController mInstance;
-        private RequestQueue mRequestQueue;
+    public static final String TAG = AppController.class.getSimpleName();
+    private static AppController mInstance;
+    private RequestQueue mRequestQueue;
 
 
     public static synchronized AppController getInstance() {
             Log.d("getInstance", "before the log.d");
             Log.d("getInstance", "mInstance: " + mInstance.toString());
             return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("onCreate", "inside AppController");
+        mInstance = this;
+    }
+
+    public RequestQueue getRequestQueue() {
+        Log.d("getRequestQueue", "inside here");
+
+
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+            Log.d("getRequestQueue",
+                    "it was null going for true branch mRequestQueue: " + mRequestQueue.toString());
+
+        } else {
+            Log.d("getRequestQueue", "going for else with mRequestQueue: " +
+                    mRequestQueue.toString());
         }
 
-        @Override
-        public void onCreate() {
-            super.onCreate();
-            Log.d("onCreate", "inside AppController");
-            mInstance = this;
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+
+        Log.d("addToRequestQueue", "inside here - 2 argument version");
+        Log.d("addToRequestQueue", "req: " + req.toString());
+        Log.d("addToRequestQueue", "tag: " + tag.toString());
+
+
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+
+    public <T> void addToRequestQueue(Request<T> req) {
+
+        Log.d("addToRequestQueue", "inside here - single argument version");
+        Log.d("addToRequestQueue", "req: " + req.toString());
+
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
         }
-
-        public RequestQueue getRequestQueue() {
-            Log.d("getRequestQueue", "inside here");
-
-
-            if (mRequestQueue == null) {
-                mRequestQueue = Volley.newRequestQueue(getApplicationContext());
-                Log.d("getRequestQueue",
-                        "mRequestQueue: " + mRequestQueue.toString());
-
-            } else {
-                Log.d("getRequestQueue", "going for else with mRequestQueue: " +
-                        mRequestQueue.toString());
-            }
-
-            return mRequestQueue;
-        }
-
-        public <T> void addToRequestQueue(Request<T> req, String tag) {
-
-            Log.d("addToRequestQueue", "inside here - 2 argument version");
-            Log.d("addToRequestQueue", "req: " + req.toString());
-            Log.d("addToRequestQueue", "tag: " + tag.toString());
-
-
-            req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-            getRequestQueue().add(req);
-        }
-
-
-        public <T> void addToRequestQueue(Request<T> req) {
-
-            Log.d("addToRequestQueue", "inside here - single argument version");
-            Log.d("addToRequestQueue", "req: " + req.toString());
-
-            req.setTag(TAG);
-            getRequestQueue().add(req);
-        }
-
-        public void cancelPendingRequests(Object tag) {
-            if (mRequestQueue != null) {
-                mRequestQueue.cancelAll(tag);
-            }
-        }
-
+    }
 }
--- app/src/main/java/android/benedetto/com/newsapp2/data/NewsArticleData.java
+++ app/src/main/java/android/benedetto/com/newsapp2/data/NewsArticleData.java
@@ -21,62 +21,65 @@ public class NewsArticleData {
 
 
     public void getNews(final NewsArticleListAsyncResponse callBack) {
-            String url = "https://content.guardianapis.com/search?q=infance&api-key=33a2e54f-afd9-4dca-bc32-6ec3de85deca&order-by=newest";
+        String url = "https://content.guardianapis.com/search?q=infance&api-key=33a2e54f-afd9-4dca-bc32-6ec3de85deca&order-by=newest";
 
-            Log.d("getNews", "inside here");
+        Log.d("getNews", "inside here");
 
-            // singleton only use one instance
-            // mistake nr. 2: you were using a different Volley in the gradle file
-            //     implementation 'com.android.volley:volley:1.1.0'
-            // instead of
-            //     implementation 'com.dubsmash.volley:library:2.0.1'
-            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
-                    url, null, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
+        // singleton only use one instance
+        // mistake nr. 2: you were using a different Volley in the gradle file
+        //     implementation 'com.android.volley:volley:1.1.0'
+        // instead of
+        //     implementation 'com.dubsmash.volley:library:2.0.1'
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
 
-                    Log.d("onResponse", "response: " + response.toString());
+                Log.d("onResponse", "response: " + response.toString());
 
-                    for (int i = 0; i < response.length(); i++) {
-                        try {
-                            JSONObject quoteObj = response.getJSONObject(i);
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject quoteObj = response.getJSONObject(i);
 
 //                            Quote quote = new Quote();
 //                            quote.setQuote(quoteObj.getString("quote"));
 //                            quote.setAuthor(quoteObj.getString("name"));
 
-                            Log.d("getQuote", quoteObj.getString("name"));
+                        Log.d("getQuote", quoteObj.getString("name"));
 
 //                            quoteArrayList.add(quote);
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-
-                    // only when the HTTP call is done, we call the callback method
-                    if (null != callBack) callBack.processFinished(news);
-
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
 
-                }
-            });
+                // only when the HTTP call is done, we call the callback method
+                if (null != callBack) callBack.processFinished(news);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
 
 
-            Log.d("getNews", "about to call AppController.getInstance().addToRequestQueue(jsonArrayRequest);");
-            Log.d("getNews", "jsonArrayRequest: " + jsonArrayRequest.toString());
-            Log.d("getNews", "after print of jsonArrayRequest");
+        Log.d("getNews", "about to call AppController.getInstance().addToRequestQueue(jsonArrayRequest);");
+        Log.d("getNews", "jsonArrayRequest: " + jsonArrayRequest.toString());
+        Log.d("getNews", "after print of jsonArrayRequest");
 
 
-            // for the second time you have spent a lot of time chasing the
-            // null value exception at this point both times caused by
-            // getInstance() because the entry for AppController was missing from the
-            // manifest file
-            AppController.getInstance().addToRequestQueue(jsonArrayRequest);
-        }
+        // for the second time you have spent a lot of time chasing the
+        // null value exception at this point both times caused by
+        // getInstance() because the entry for AppController was missing from the
+        // manifest file
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+    }
 }
 
 
